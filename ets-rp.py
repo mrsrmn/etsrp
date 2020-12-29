a510fdc6a07fde75b3264e5af6aaa7f62f4e1498
MakufonSkifto <63517174+emirsurmen@users.noreply.github.com>
2020-12-29 23:37:53 +0300

    bug fixes

--- ets-rp.py
+++ ets-rp.py
@@ -2,11 +2,13 @@ from pypresence import Presence
 import time
 import epoch
 import wmi
+import datetime
 
 f = wmi.WMI()
+RPC = Presence("793495110440583178")
 
 print("╔════════════════════════════════════════════════════════════════════════════╗\n"
-      "║ Welcome to ETSRP v1!                                                       ║\n"
+      "║ Welcome to ETSRP v1.2!                                                       ║\n"
       "║                                                                            ║\n"
       "║ ETSRP provides a better version of ETS2's original Discord Rich Presence   ║\n"
       "║                                                                            ║\n"
@@ -23,16 +25,21 @@ while True:
     if "eurotrucks2.exe" in li:
         now = epoch.now()
 
-        RPC = Presence("793495110440583178")
         RPC.connect()
 
         RPC.update(state="Driving in Europe", large_image="ets", large_text="RP Mod by MakufonSkifto",
                    small_image="eu", start=now)
-        print("Showing RP")
 
+        now_datetime = datetime.datetime.now()
+        print(f"[INFO {now_datetime.strftime('%H:%M:%S')}]: Showing RP")
         time.sleep(15)
 
     else:
-        print("No running ETS2 detected")
+        now_datetime = datetime.datetime.now()
+        try:
+            RPC.close()
+            print(f"[INFO {now_datetime.strftime('%H:%M:%S')}]: No running ETS2 detected, closing RP")
+        except AttributeError:
+            print(f"[INFO {now_datetime.strftime('%H:%M:%S')}]: No running ETS2 detected")
 
-    time.sleep(15)
+    time.sleep(10)
